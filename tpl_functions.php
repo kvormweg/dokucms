67d0a7eaac3ed14f46efbdef4582106f1bbc843b
kvormweg <klaus.vormweg@gmx.de>
2016-03-20 11:48:56 +0100

    fix for Dokuwiki namespace default linking

--- tpl_functions.php
+++ tpl_functions.php
@@ -26,14 +26,14 @@ function _tpl_sidebar() {
     $tpl = $conf['template'];
 
     if($conf['tpl'][$tpl]['sidebar']== 'file')  {
-        $ns_sb = _getNsSb($ID);
+        $ns_sb = _tpl_getNsSb($ID);
         if($ns_sb && auth_quickaclcheck($ns_sb) >= AUTH_READ) {
             echo '<div class="sidebar_box">', DOKU_LF;
-            echo p_sidebar_xhtml($ns_sb), DOKU_LF;
+            echo _tpl_sidebar_xhtml($ns_sb), DOKU_LF;
             echo '</div>', DOKU_LF;
          } elseif(@file_exists(wikiFN($pname)) && auth_quickaclcheck($pname) >= AUTH_READ) {
             echo '<div class="sidebar_box">', DOKU_LF;
-            echo p_sidebar_xhtml($pname), DOKU_LF;
+            echo _tpl_sidebar_xhtml($pname), DOKU_LF;
             echo '</div>', DOKU_LF;
         } else {
             echo '<div class="sidebar_box">', DOKU_LF;
@@ -42,7 +42,7 @@ function _tpl_sidebar() {
 			   }
    } else {
     			echo '<div class="sidebar_box">', DOKU_LF;
-    			echo '  ', p_index_xhtml($ID), DOKU_LF;
+    			echo '  ', _tpl_index_xhtml($ID), DOKU_LF;
     			echo '</div>', DOKU_LF;
 	 }	
 }
@@ -50,7 +50,7 @@ function _tpl_sidebar() {
 /**
  * searches for namespace sidebars
  */
-function _getNsSb($id) {
+function _tpl_getNsSb($id) {
     $pname = 'sidebar';
     $ns_sb = '';
     $path  = explode(':', $id);
@@ -70,7 +70,7 @@ function _getNsSb($id) {
  * shows a edit button if the user has enough rights
  *
  */
-function p_sidebar_xhtml($sb) {
+function _tpl_sidebar_xhtml($sb) {
   global $conf;
   $tpl = $conf['template'];
   $data = p_wiki_xhtml($sb,'',false);
@@ -88,7 +88,7 @@ function p_sidebar_xhtml($sb) {
  * Renders the Index
  *
  */
-function p_index_xhtml($ns) {
+function _tpl_index_xhtml($ns) {
   require_once(DOKU_INC.'inc/search.php');
   global $conf;
   global $ID;
@@ -109,6 +109,7 @@ function p_index_xhtml($ns) {
   $ns  = utf8_encodeFN(str_replace(':','/',$ns));
 
   $data = array();
+  $data2 = array();
   search($data,$conf['datadir'],'search_index',array('ns' => $ns));
   $i = 0;
   $cleanindexlist = array();
@@ -124,7 +125,7 @@ function p_index_xhtml($ns) {
 	foreach($data as $item) {
     if($conf['tpl'][$tpl]['cleanindex']) {
       if($item['id'] == 'playground' or $item['id'] == 'wiki') {
-        unset($data[$i]);
+        continue;
       }
       if(count($cleanindexlist)) {
       	if(strpos($item['id'], ':')) {
@@ -133,17 +134,24 @@ function p_index_xhtml($ns) {
       		$tmpitem = $item['id'];
       	}
 	      if(in_array($tmpitem, $cleanindexlist)) {
-	        unset($data[$i]);
+	        continue;
 	      }
 			}
     }
-    if($item['id'] == 'sidebar' or $item['id'] == $start or preg_match('/:'.$start.'$/',$item['id'])) {
-      unset($data[$i]);
+    if($item['id'] == 'sidebar' or $item['id'] == $start 
+       or preg_match('/:'.$start.'$/',$item['id'])
+       or preg_match('/(\w+):\1$/',$item['id'])) {
+      continue;
     }
-    $i++;
+    if($item['type'] == 'f' and array_key_exists($item['id'], $data2)) {
+      $data2[$item['id']]['type'] = 'd';
+      $data2[$item['id']]['ns'] = $item['id'];
+      continue;
+    } 
+    $data2[$item['id']] = $item;
   }  
 # echo index with empty items removed  
-  echo html_buildlist($data,'idx','_html_list_index','html_li_index');
+  echo html_buildlist($data2,'idx','_tpl_html_list_index','_tpl_html_li_index');
 }
 
 /**
@@ -152,21 +160,50 @@ function p_index_xhtml($ns) {
  * User function for html_buildlist()
  *
  */
-function _html_list_index($item){
-  global $ID;
+function _tpl_html_list_index($item){
   global $conf;
   $ret = '';
   if($item['type']=='d'){
     if(@file_exists(wikiFN($item['id'].':'.$conf['start']))) {
-      $ret .= html_wikilink($item['id'].':'.$conf['start']);
+      $ret .= html_wikilink($item['id'].':'.$conf['start'], $item['title']);
+    } elseif(@file_exists(wikiFN($item['id'].':'.$item['id']))) {
+      $ret .= html_wikilink($item['id'].':'.$item['id'], $item['title']);
     } else {
-      $ret .= html_wikilink($item['id'].':');
+      $ret .= html_wikilink($item['id'].':', $item['title']);
     }
   } else {
-    $ret .= html_wikilink(':'.$item['id']);
+    $ret .= html_wikilink(':'.$item['id'], $item['title']);
   }
   return $ret;
 }
+/**
+ * Index List item
+ *
+ * This user function is used in html_buildlist to build the
+ * <li> tags for namespaces when displaying the page index
+ * it gives different classes to opened or closed "folders"
+ *
+ * @author Andreas Gohr <andi@splitbrain.org>
+ *
+ * @param array $item
+ * @return string html
+ */
+function _tpl_html_li_index($item){
+  global $INFO;
+
+  $class = '';
+  $id = '';
+
+  if($item['type'] == "f"){
+    // scroll to the current item
+    return '<li class="level'.$item['level'].$class.'" '.$id.'>';
+  } elseif($item['open']) {
+    return '<li class="open">';
+  } else {
+    return '<li class="closed">';
+  }
+}
+
 
 # dokucms modified version of pageinfo 
 function _tpl_pageinfo(){
